41e1636ac9f1ec66b3066e2fa9708b3946288a52
RobinAxelsson <63591629+RobinAxelsson@users.noreply.github.com>
2021-09-21 10:27:31 +0200

    added start-stop

--- azure.sh
+++ azure.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 # az login -u <username> -p <password>
-
+# read -sp "Azure password: " AZ_PASS && echo && az login -u <username> -p $AZ_PASS
 run() {
   az ad sp create-for-rbac --name "FunctionCalculator" --role contributor \
     --scopes /subscriptions/$1/resourceGroups/$2 \
     --sdk-auth
 }
-run $(cat ./secrets/sub-id.secret) $(cat ./secrets/res-group.secret)
+# run $(cat ./secrets/sub-id.secret) $(cat ./secrets/res-group.secret)
+
+az functionapp stop --name "FunctionCalculator" --resource-group $(cat ./secrets/res-group.secret)
+# az functionapp start --name "FunctionCalculator" --resource-group $(cat ./secrets/res-group.secret)
 
 # az ad sp create-for-rbac --name "myApp" --role contributor \
 #   --scopes /subscriptions/{subscription-id}/resourceGroups/{resource-group} \
